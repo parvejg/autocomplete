3c5e0f571250921bf8a8880ce67beb7375c1e0fc
parvejg <parvezpg321@gmail.com>
2023-12-05 23:17:02 +0530

    create a autoComplete component

--- src/AutoCompletePage.js
+++ src/AutoCompletePage.js
@@ -1,1 +1,9 @@
 import "./AutocompletePage.css";
+export const Autocomplete = () => {
+  return (
+    <div className="main-container">
+      <h1>Autocomplete</h1>
+      <div className="autocomplete-wrapper"></div>
+    </div>
+  );
+};
