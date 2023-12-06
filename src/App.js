639e010b1baaa7245787cdbb29190f503c395051
parvejg <parvezpg321@gmail.com>
2023-12-06 22:28:11 +0530

    set input default value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Autocomplete } from "./AutoCompletePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Autocomplete />
+    </div>
+  );
 }
 
 export default App;
--- src/AutoCompletePage.js
+++ src/AutoCompletePage.js
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import "./AutocompletePage.css";
+import { countries } from "./ContriesArrary";
 export const Autocomplete = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [clickedCountry, setClickedCountry] = useState("");
+  const filteredArray = countries?.filter((item) =>
+    item.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  console.log(clickedCountry);
   return (
     <div className="main-container">
       <h1>Autocomplete</h1>
-      <div className="autocomplete-wrapper"></div>
+      <p className="start-txt">Start typing:</p>
+      <div className="autocomplete-wrapper">
+        <input
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setClickedCountry(e.target.value);
+          }}
+          type="text"
+          className="autocomplete-input"
+          placeholder="Country"
+          value={clickedCountry ? clickedCountry : ""}
+        />
+        <button className="submit-btn">Submit</button>
+      </div>
+
+      <div>
+        {inputValue &&
+          filteredArray?.map((countryName) => {
+            return (
+              <button
+                type="button"
+                className="countryName-wrapper"
+                onClick={(e) => {
+                  setClickedCountry(e.target.value);
+                  console.log(e.target.value);
+                }}
+                value={countryName}
+              >
+                {countryName}
+              </button>
+            );
+          })}
+      </div>
     </div>
   );
 };
